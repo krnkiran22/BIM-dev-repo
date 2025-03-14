6a4ca828101d0ebf8688fc69d440e9f16c03b586
krnkiran22 <kirandev2210@gmail.com>
2025-03-14 14:42:46 +0530

    bim developer repo using react

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "BIM DEVELOPER",
+          
+          "HARINI KU ANNA",
+      
         ],
         autoStart: true,
         loop: true,
