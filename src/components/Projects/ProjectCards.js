f34f5e4535dbed00d7bdaf05637d9cbf60afd3e5
Hariniha <harinikannan302004@gmail.com>
2025-03-21 11:07:42 +0530

    Updation

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+// import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
@@ -13,15 +13,15 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
+        {/* <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
+        </Button> */}
         {"\n"}
         {"\n"}
 
         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
+{/* 
         {!props.isBlog && props.demoLink && (
           <Button
             variant="primary"
@@ -32,9 +32,10 @@ function ProjectCards(props) {
             <CgWebsite /> &nbsp;
             {"Demo"}
           </Button>
-        )}
+        )} */}
       </Card.Body>
     </Card>
+    
   );
 }
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,19 +3,19 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import VGP4 from "../../Assets/Projects/VGP4.png";
-import VGP6 from "../../Assets/Projects/VGP6.png";
+import VGP1 from "../../Assets/Projects/VGP1.png";
 import VGP2 from "../../Assets/Projects/VGP2.png";
 import rajesh2 from "../../Assets/Projects/rajesh2.png";
-import rajesh4 from "../../Assets/Projects/rajesh4.png";
-import rajesh5 from "../../Assets/Projects/rajesh5.png";
-import OASIS2 from "../../Assets/Projects/OASIS2.png";
+import rajesh from "../../Assets/Projects/rajesh.png";
+import rajesh3 from "../../Assets/Projects/rajesh3.png";
+import OASIS1 from "../../Assets/Projects/OASIS1.png";
 import OASIS3 from "../../Assets/Projects/OASIS3.png";
 import OASIS from "../../Assets/Projects/OASIS.png";
 import infinity2 from "../../Assets/Projects/infinity2.png";
 import infinity from "../../Assets/Projects/infinity.png";
 import infinity4 from "../../Assets/Projects/infinity4.png";
 import jeffry4 from "../../Assets/Projects/jeffry4.png";
-import jeffry1 from "../../Assets/Projects/jeffry1.png";
+import jeffry3 from "../../Assets/Projects/jeffry3.png";
 import jeffry2 from "../../Assets/Projects/jeffry2.png";
 import Aravallilawn1 from "../../Assets/Projects/Aravallilawn1.png";
 import Aravallilawn from "../../Assets/Projects/Aravallilawn.png";
@@ -45,21 +45,20 @@ function Projects() {
             <ProjectCard
               imgPath={VGP4}
               isBlog={false}
+              
               title="VGP Pointcloud"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+         
             />
           </Col>
 
           <Col md={4} className="project-card" >
             <ProjectCard
-              imgPath={VGP6}
+              imgPath={VGP1}
               isBlog={false}
-              title="VGP Model"
+              title="VGP Revit Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+             
             />
           </Col>
 
@@ -67,10 +66,19 @@ function Projects() {
             <ProjectCard
               imgPath={VGP2}
               isBlog={false}
-              title="VGP Model"
+              title="VGP Revit Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+                           
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={rajesh}
+              isBlog={false}
+              title="Rajesh Building Pointcloud "
+              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
+            
             />
           </Col>
 
@@ -78,45 +86,20 @@ function Projects() {
             <ProjectCard
               imgPath={rajesh2}
               isBlog={false}
-              title="Building Pointcloud "
+              title="Rajesh Building Floorplan"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+       
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={rajesh4}
+              imgPath={rajesh3}
               isBlog={false}
-              title="Building Model"
-              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={rajesh5}
-              isBlog={false}
-              title="Building Model"
+              title="Rajesh Building Revit Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"       
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={OASIS2}
-              isBlog={false}
-              title="OASIS-Restaurant Pointcloud"
-              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"       
+                  
             />
           </Col>
 
@@ -126,9 +109,18 @@ function Projects() {
               isBlog={false}
               title="OASIS-Restaurant Pointcloud"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
+      
+            />
+          </Col>
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"       
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={OASIS1}
+              isBlog={false}
+              title="OASIS-Restaurant Revit Model"
+              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
+
+                     
             />
           </Col>
 
@@ -136,12 +128,11 @@ function Projects() {
             <ProjectCard
               imgPath={OASIS}
               isBlog={false}
-              title="OASIS-Restaurant Model"    
+              title="OASIS-Restaurant Revit Model"    
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+                  
             />
           </Col>
 
@@ -152,8 +143,7 @@ function Projects() {
               title="Inifity Pool Pointcloud"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+         
             />
           </Col>
 
@@ -161,11 +151,10 @@ function Projects() {
             <ProjectCard
               imgPath={infinity}
               isBlog={false}
-              title="Inifity Pool Floorplan"
+              title="Inifity Pool Revit Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+                    
             />
           </Col>
 
@@ -173,11 +162,21 @@ function Projects() {
             <ProjectCard
               imgPath={infinity4}
               isBlog={false}
-              title="Inifity Pool Model"
+              title="Inifity Pool Sketchup Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+                 
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={jeffry3}
+              isBlog={false}
+              title="Jeffry Building Pointcloud"
+              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
+
+                   
             />
           </Col>
 
@@ -185,23 +184,10 @@ function Projects() {
             <ProjectCard
               imgPath={jeffry4}
               isBlog={false}
-              title="Building Pointcloud"
+              title="Jeffry Building Floorplan "
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={jeffry1}
-              isBlog={false}
-              title="Building Model"
-              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"       
+                   
             />
           </Col>
 
@@ -209,11 +195,10 @@ function Projects() {
             <ProjectCard
               imgPath={jeffry2}
               isBlog={false}
-              title="Building Model"
+              title="Jeffry Building Revit Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+             
             />
           </Col>
 
@@ -224,8 +209,7 @@ function Projects() {
               title="Ararvalli Lawn Pointcloud"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+               
             />
           </Col>
 
@@ -233,11 +217,10 @@ function Projects() {
             <ProjectCard
               imgPath={Aravallilawn}
               isBlog={false}
-              title="Ararvalli Lawn Floorplan"
+              title="Ararvalli Lawn Revit Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+                    
             />
           </Col>
 
@@ -245,23 +228,10 @@ function Projects() {
             <ProjectCard
               imgPath={Aravallilawn3}
               isBlog={false}
-              title="Ararvalli Lawn Model"
+              title="Ararvalli Lawn Sketchup Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"     
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={yercaud3}
-              isBlog={false}
-              title="Yercaud Villa Pointcloud"
-              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
-
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+                 
             />
           </Col>
 
@@ -272,8 +242,18 @@ function Projects() {
               title="Yercaud Villa Pointcloud"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"      
+                   
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={yercaud3}
+              isBlog={false}
+              title="Yercaud Villa Floorplan"
+              description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
+
+                  
             />
           </Col>
 
@@ -281,11 +261,10 @@ function Projects() {
             <ProjectCard
               imgPath={yercaud2}
               isBlog={false}
-              title="Yercaud Villa Model"
+              title="Yercaud Villa Revit Model"
               description="Processed point cloud data to generate accurate floor plans and 3D BIM models, ensuring precise as-built documentation."
 
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              demoLink="https://blogs.soumya-jit.tech/"       
+                   
             />
           </Col>
 
